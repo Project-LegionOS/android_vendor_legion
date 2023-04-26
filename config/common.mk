PixelExperience/vendor_aosp

vendor: Add framework compatibility matrix for Lineage HALs.
 * Cover all possible Lineage HALs here instead of covering
   them in the device specific manifests.

Change-Id: Id7f7d6eb1f14190a061537cebfabd339af9b1620

config/common.mk
@@ -103,6 +103,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     vendor/aosp/overlay/common \
     vendor/aosp/overlay-pixel/common
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aosp/config/device_framework_matrix.xml
+
 # TouchGestures
 PRODUCT_PACKAGES += \
     TouchGestures

Project-LegionOS/android_vendor_legion

Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

config/common.mk
@@ -90,6 +90,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/legion/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
+
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/legion/config/device_framework_matrix.xml
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 